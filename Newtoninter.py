b4c4967f6dc085f0842ae74eae691ab99f553b87
JuanGutierrez85 <87103409+JuanGutierrez85@users.noreply.github.com>
2021-07-07 19:07:06 -0600

    Add files via upload
    Modulos usados en FOVEA

---
+++ Newtoninter.py
@@ -0,0 +1,15 @@
+
+
+
+def evalPoly(a,xData,x):
+     n = len(xData) - 1 # grado del polinomio
+     p = a[n]
+     for k in range(1,n+1):
+      p = a[n-k] + (x -xData[n-k])*p
+     return p
+def coeffts(xData,yData):
+     m = len(xData) # n ́umero de datos
+     a = yData.copy()
+     for k in range(1,m):
+      a[k:m] = (a[k:m] - a[k-1])/(xData[k:m] - xData[k-1])
+     return a
